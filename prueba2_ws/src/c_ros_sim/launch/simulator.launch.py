755c8442e687d1860a7d8f02ae6a79e404ebb7be
Oswiwi <osvaldo.prodigy@gmail.com>
2026-04-14 17:09:13 -0600

    Simulación estable. Cámara simulada funcional con parámetros ov5647. Nodo de detección de pelota calibrado con menos del 5% de error.

--- prueba2_ws/src/c_ros_sim/launch/simulator.launch.py
+++ prueba2_ws/src/c_ros_sim/launch/simulator.launch.py
@@ -47,7 +47,9 @@ def generate_launch_description():
             '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
             '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
             '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
-            '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan'
+            '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
+            '/camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image', # Imagen Raw
+            '/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo'
         ],
         output='screen'
     )
